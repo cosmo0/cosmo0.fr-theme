290e7c2600fa4e1f1356e168d78fecff4671c347
cosmo0 <thomas.broust@gmail.com>
2020-09-30 10:14:02 +0200

    Utilisation de Plyr

--- functions.php
+++ functions.php
@@ -16,10 +16,17 @@ define( 'CHILD_THEME_COSMO0_VERSION', '1.0.0' );
 /**
  * Enqueue styles
  */
-function child_enqueue_styles() {
-
+function child_custom_styles() {
 	wp_enqueue_style( 'cosmo0-theme-css', get_stylesheet_directory_uri() . '/style.css', array('astra-theme-css'), CHILD_THEME_COSMO0_VERSION, 'all' );
-
+	wp_enqueue_style( 'plyr-css', get_stylesheet_directory_uri() . '/css/plyr.css', array('astra-theme-css'), '', 'all' );
 }
 
-add_action( 'wp_enqueue_scripts', 'child_enqueue_styles', 15 );
+/**
+ * Enqueue scripts
+ */
+function child_custom_scripts() {
+    wp_enqueue_script( 'plyr-js', get_stylesheet_directory_uri() . '/js/plyr.polyfilled.min.js', array( 'jquery' ),'',true );
+}
+
+add_action( 'wp_enqueue_scripts', 'child_custom_styles', 15 );
+add_action( 'wp_enqueue_scripts', 'child_custom_scripts' );
